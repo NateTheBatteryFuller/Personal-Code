dd46b38df53b8e7e887109163899301af92292b3
Nate Fuller <natefuller.jesussaves@gmail.com>
2016-04-05 13:42:45 -0400

    Added files via upload

---
+++ CSCE145/lab9/SavingsAccount.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class SavingsAccount {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String s = "name";
+		String t = "owner";
+		double balance = 0;
+		System.out.print("Please input your balance: ");
+		Scanner input = new Scanner(System.in);
+		double b = input.nextInt();
+		System.out.print("Your balance is: " +b+ " dollars");
+		
+		
+		
+		
+		
+	}
+
+}
